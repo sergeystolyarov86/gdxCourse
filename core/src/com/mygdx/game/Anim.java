f3792184c8b4c0d1564c2c8bbf884c0ca4a738b6
Сергей Сергеевич Столяров <sergey_stolyarov@mail.ru>
2022-08-19 16:46:12 +0300

    вопрос в комментарии

---
+++ core/src/com/mygdx/game/Anim.java
@@ -0,0 +1,57 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Anim {
+    private Texture img;
+    private Animation<TextureRegion> anm;
+    private float time;
+
+    public Anim(String name, int col, int row, Animation.PlayMode playMode) {
+        img = new Texture(name);
+        TextureRegion region0 = new TextureRegion(img);
+        int xCnt = region0.getRegionWidth() / col;
+        int yCnt = region0.getRegionHeight() / row;
+
+        TextureRegion[][] regions0 = region0.split(xCnt, yCnt);
+
+        TextureRegion[] region1 = new TextureRegion[regions0.length * regions0[0].length];
+        int cnt = 0;
+        for (int i = 0; i < regions0.length; i++) {
+            for (int j = 0; j < regions0[0].length; j++) {
+                region1[cnt++] = regions0[i][j];
+            }
+        }
+
+        anm = new com.badlogic.gdx.graphics.g2d.Animation<TextureRegion>(1 /20f, region1);
+        anm.setPlayMode(playMode);
+    }
+
+    public TextureRegion getFrame() {
+        return anm.getKeyFrame(time);
+    }
+
+    public void setTime(float time) {
+        this.time += time;
+    }
+
+    public void zeroTime() {
+        this.time += 0;
+    }
+
+    public boolean isAnimationOver() {
+        return anm.isAnimationFinished(time);
+    }
+
+    public void setPlayMode(Animation.PlayMode playMode){
+        anm.setPlayMode(playMode);
+    }
+
+    public void dispose(){
+        img.dispose();
+    }
+
+
+}
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,38 +2,73 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	int clk;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("java.jpg");
-	}
+    SpriteBatch batch;
+    int moveX;
+    Anim animation;
+    boolean isInFrame = true;
+    boolean isRevers = true;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(0, 0, 1, 1);
-		float x = Gdx.input.getX()-img.getWidth()/2;
-		float y = Gdx.graphics.getHeight() - Gdx.input.getY() - img.getHeight()/2;
-		if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) clk++;
-		Gdx.graphics.setTitle("Clicked "+clk+" times");
-		batch.begin();
-		//batch.draw(img, 0, 0);
-		batch.draw(img,x,y);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        animation = new Anim("run.png", 5, 2, Animation.PlayMode.LOOP);
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(1, 1, 1, 1);
+
+        animation.setTime(Gdx.graphics.getDeltaTime());
+        float x = Gdx.input.getX() - animation.getFrame().getRegionWidth() ;
+        float y = Gdx.graphics.getHeight() - Gdx.input.getY() - animation.getFrame().getRegionHeight() ;
+
+
+        batch.begin();
+        runMan();
+        batch.draw(animation.getFrame(), moveX, 0);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        animation.dispose();
+    }
+
+
+    /**
+     мой персонаж добегает до 1/4 экрана(сократил расстояние,чтобы меньше ждать) ,разворачивается и бежит
+     в развороте в начало.добегает до начала и бежит обратно,но не разворачивается.в блок if в 53 строке заходит,
+     но не возвращает персонажа в начальное положение. подскажите,пожалуйста,в чем ошибка?
+     */
+
+    public void runMan() {
+        if (!animation.getFrame().isFlipX() && !isRevers){ animation.getFrame().flip(true, false);
+            System.out.println("flipX=true");}
+        if (animation.getFrame().isFlipX() || isRevers) {animation.getFrame().flip(false, false);
+            System.out.println("flipX=false");
+            System.out.println(animation.getFrame().isFlipX());}
+
+        int finish = (Gdx.graphics.getWidth() - animation.getFrame().getRegionWidth()) / 4;
+        if (moveX == finish) {
+            isRevers = false;
+            isInFrame = false;
+        }
+        if (moveX == 0 && animation.getFrame().isFlipX()) {
+            isRevers = true;
+            isInFrame = true;
+        }
+        if (isInFrame) {
+            moveX++;
+        } else {
+            moveX--;
+        }
+    }
 }
+
